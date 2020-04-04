7dcc8b9c7880ad24ea17788db50d346e22c50730
akhiljayan29.aj <akhiljayan29.aj@gmail.com>
2020-04-04 16:50:11 +0530

    end sections

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Initializee and add the map
+function initMap() {
+    //Your Location
+    const loc = { lat: 42.361145, lng: -71.057083 };
+    // Centered map on Location
+    const map = new google.maps.Map(document.querySelector('.map') , {
+        zoom: 14,
+        center: loc
+    });
+    // The marker, positioned at location
+    const marker = new google.maps.Marker({ position: loc, map:map })
+}
